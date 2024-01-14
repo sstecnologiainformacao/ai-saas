8d09099c60b2a8e5fdaf8365c78dc8c7b3f2d5c9
João Lucas dos Santos <j.lucas@resolution.de>
2024-01-14 16:19:45 -0300

    Working in progress. Sidebar and navbar has been added to the project.

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,8 @@
-import { Button } from '@/components/ui/button';
-import { UserButton } from '@clerk/nextjs';
 
 const Dashboard = () => {
   return (
     <div>
       Dashboard page
-      <UserButton afterSignOutUrl='/'/>
     </div>
   );
 }
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
 const DashboardLayout = ({
     children
 } : {
@@ -5,9 +8,13 @@ const DashboardLayout = ({
 }) => {
     return (
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div>Hello sidebar</div>
+            <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+                <Sidebar />
             </div>
+            <main className="md:pl-72">
+                <Navbar />
+                {children}
+            </main>
         </div>
     );
 }
